c70c8cb6d92346721e699dfe7966e395d937deeb
Bryanjay Bodino <bryanjaybodino@gmail.com>
2024-09-06 10:20:17 +0800

    Secured MySQL Password and Username

--- DatabaseBackup/Main.Designer.cs
+++ DatabaseBackup/Main.Designer.cs
@@ -30,7 +30,7 @@ namespace DatabaseBackup
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
@@ -362,6 +362,7 @@ namespace DatabaseBackup
             this.textBox_mysql_password.Name = "textBox_mysql_password";
             this.textBox_mysql_password.Size = new System.Drawing.Size(292, 31);
             this.textBox_mysql_password.TabIndex = 22;
+            this.textBox_mysql_password.UseSystemPasswordChar = true;
             // 
             // label1
             // 
@@ -381,6 +382,7 @@ namespace DatabaseBackup
             this.textBox_mysql_username.Name = "textBox_mysql_username";
             this.textBox_mysql_username.Size = new System.Drawing.Size(292, 31);
             this.textBox_mysql_username.TabIndex = 20;
+            this.textBox_mysql_username.UseSystemPasswordChar = true;
             // 
             // button_choose_mysql_location
             // 
@@ -422,13 +424,13 @@ namespace DatabaseBackup
             // 
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.ColumnHeadersHeight = 29;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
